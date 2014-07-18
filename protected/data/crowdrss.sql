8422cc4251f372b20ed6f747381823dcb4b1e9ac
Ziga Berce <bercium@gmail.com>
2014-07-18 09:58:47 +0200

    Db change

--- protected/data/crowdrss.sql
+++ protected/data/crowdrss.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 17, 2014 at 04:53 PM
+-- Generation Time: Jul 18, 2014 at 09:57 AM
 -- Server version: 5.5.37-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.3
 
@@ -35,6 +35,54 @@ CREATE TABLE IF NOT EXISTS `category` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mail_click_log`
+--
+
+CREATE TABLE IF NOT EXISTS `mail_click_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail_tracking_code` int(11) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `time_clicked` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `button_name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `mail_id` (`mail_tracking_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mail_log`
+--
+
+CREATE TABLE IF NOT EXISTS `mail_log` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `tracking_code` int(11) NOT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `subscription_id` int(11) DEFAULT NULL,
+  `time_send` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `time_open` timestamp NULL DEFAULT NULL,
+  `extra_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tracking_code` (`tracking_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orig_category`
+--
+
+CREATE TABLE IF NOT EXISTS `orig_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `platform`
 --
 
@@ -61,7 +109,7 @@ INSERT INTO `platform` (`id`, `name`) VALUES
 CREATE TABLE IF NOT EXISTS `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `platform_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
+  `orig_category_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
   `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -72,11 +120,30 @@ CREATE TABLE IF NOT EXISTS `project` (
   `creator` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `creator_created` int(11) DEFAULT NULL,
   `creator_backed` int(11) DEFAULT NULL,
-  `goal` int(11) DEFAULT NULL,
+  `goal` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `type_of_funding` int(11) DEFAULT NULL,
+  `time_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `platform_id` (`platform_id`),
-  KEY `category_id` (`category_id`)
+  KEY `orig_category_id` (`orig_category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription`
+--
+
+CREATE TABLE IF NOT EXISTS `subscription` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `platform` int(11) DEFAULT NULL,
+  `category` int(11) DEFAULT NULL,
+  `rss` tinyint(1) DEFAULT '0',
+  `email_time` int(11) DEFAULT NULL,
+  `email_limit` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
@@ -84,11 +151,23 @@ CREATE TABLE IF NOT EXISTS `project` (
 --
 
 --
+-- Constraints for table `mail_click_log`
+--
+ALTER TABLE `mail_click_log`
+  ADD CONSTRAINT `mail_click_log_ibfk_1` FOREIGN KEY (`mail_tracking_code`) REFERENCES `mail_log` (`tracking_code`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orig_category`
+--
+ALTER TABLE `orig_category`
+  ADD CONSTRAINT `orig_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
 -- Constraints for table `project`
 --
 ALTER TABLE `project`
-  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`platform_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `project_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `project_ibfk_2` FOREIGN KEY (`orig_category_id`) REFERENCES `orig_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`platform_id`) REFERENCES `platform` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
