8796fc48b439537933e23771886fc9e700575dcc
Ziga Berce <bercium@gmail.com>
2014-08-11 18:16:28 +0200

    Daily digest cron

--- protected/commands/MailerCommand.php
+++ protected/commands/MailerCommand.php
@@ -2,43 +2,94 @@
 
 class MailerCommand extends CConsoleCommand{
 
+  private function createSQL($sub, $days = 1){
+    $sql = '';
+    if ($sub->category){
+    //if ($this->validateId($sub->category)){
+      //$orgCat = OrigCategory::model()->findAll("(category_id IN (".$this->validateId($sub->category)."))");
+      $orgCat = OrigCategory::model()->findAll("(category_id IN (".$sub->category."))");
+
+      $allCats = array();
+      foreach ($orgCat as $cat){
+        if (!in_array($cat->id, $sub->exclude_orig_category)) $allCats[$cat->id] = $cat->id;
+      }
+      $sql .= " (orig_category_id IN (".implode(',',$allCats).")) AND ";
+    }
+    if ($sub->platform) $sql .= " (platform_id IN (".$sub->platform.")) AND ";
+    //if ($this->validateId($sub->platform)) $sql .= " (platform_id IN (".$this->validateId($sub->platform).")) AND ";
+    else{
+      $platforms = Platform::model()->findAll("active = :active",array(":active"=>0));
+      $selplat = '';
+      foreach ($platforms as $platform){
+        if ($selplat) $selplat .= ',';
+        $selplat .= $platform->id;
+      }
+      if ($selplat) $sql .= " (platform_id NOT IN (".$selplat.")) AND ";
+    }
+    //$sql .= ' 1 ';
+    $hours = $days*24+3;
+    $sql .= " (time_added < DATE_ADD(NOW(),INTERVAL -3 HOUR)) AND (time_added >= DATE_ADD(NOW(),INTERVAL -".$hours." HOUR)) ";  // one day slot with 3h delay
+    if ($sub->rating > 0)  $sql .= " AND (rating = NULL OR rating >= ".$sub->rating.") ";
+
+    $sql .= " ORDER BY rating DESC, time_added ASC";
+    $sql .= " LIMIT 12";
+    
+    return $sql;
+  }
+  
+  
+  /**
+   * daily digest
+   */
 	public function actionDailyDigest(){
-    
-    $message = new YiiMailMessage;
-    $message->view = 'digest';
-    $message->subject = "Daily digest of crowdfunding projects";  // 11.6. title change
-    $message->from = Yii::app()->params['noreplyEmail'];
-    
-    // send newsletter to all in waiting list
-    $invites = Invite::model()->findAll("NOT ISNULL(`key`)");
-    foreach ($invites as $user){
-      
-      $create_at = strtotime($user->time_invited);
-      if ($create_at < strtotime('-8 week') || $create_at >= strtotime('-1 day')) continue;     
-      if (!
-          (($create_at >= strtotime('-1 week')) || 
-          (($create_at >= strtotime('-4 week')) && ($create_at < strtotime('-3 week'))) || 
-          (($create_at >= strtotime('-8 week')) && ($create_at < strtotime('-7 week'))) )
-         ) continue;      
-      
-      //set mail tracking
-      $mailTracking = mailTrackingCode($user->id);
-      $ml = new MailLog();
-      $ml->tracking_code = mailTrackingCodeDecode($mailTracking);
-      $ml->type = 'invitation-reminder';
-      $ml->user_to_id = $user->id;
-      $ml->save();
-    
-      //$activation_url = '<a href="'.absoluteURL()."/user/registration?id=".$user->key.'">Register here</a>';
-      $activation_url = mailButton("Register here", absoluteURL()."/user/registration?id=".$user->key,'success',$mailTracking,'register-button');
-      $content = "This is just a friendly reminder to activate your account on Cofinder.
-                  <br /><br />
-                  Cofinder is a web platform through which you can share your ideas with the like minded entrepreneurs, search for people to join your project or join an interesting project yourself.
-                  <br /><br />If we got your attention you can ".$activation_url."!";
-      
-      $message->setBody(array("content"=>$content,"email"=>$user->email,"tc"=>$mailTracking), 'text/html');
-      $message->setTo($user->email);
-      Yii::app()->mail->send($message);
+    $subscriptions = Subscription::model()->findAllByAttributes(array('daily_digest'=>1));
+    if ($subscriptions){
+      foreach ($subscriptions  as $sub){
+
+        $sql .= $this->createSQL($sub, 1);
+
+        // get projects
+        $projects = Project::model()->findAll($sql);
+        $count = count($projects);
+        
+        $featured = array_slice($project, 0, 4);
+        if ($count < 12) $regular = array_slice($project, 4, 8);
+        else if ($count < 8) $regular = array();
+        
+        
+        //set mail tracking
+        $tc = mailTrackingCode();
+        $ml = new MailLog();
+        $ml->tracking_code = mailTrackingCodeDecode($tc);
+        $ml->type = 'daily-digest';
+        $ml->subscription_id = $sub->id;
+        $ml->save();
+
+        // create message
+        $message = new YiiMailMessage;
+        $message->view = 'digest';
+        $message->subject = "Your Daily Dose Of Crowdfunding Projects";  // 11.6. title change
+        $message->from = Yii::app()->params['noreplyEmail'];
+
+        $title = 'Yesterdays top crowdfunding projects';
+        $content = '';
+
+        // not enough projects
+        if ($count < 4){
+          $content = 'We found just a few projects. <br />Maybe your rules are set too stric? Consider editing your feed.<hr>';
+        }
+
+        $editLink = Yii::app()->createAbsoluteUrl("site/index",array("id"=>$sub->hash));
+
+        $message->setBody(array("tc"=>$tc,"user_id"=>$sub->id,
+                                "content"=>$content, "title"=>$title,
+                                "featuredProjects"=>$featured, "projects"=>$regular,
+                                "showEdit"=>true,"editLink"=>$editLink
+                                ), 'text/html');
+        $message->setTo($user->email);
+        Yii::app()->mail->send($message);
+        
+      }
     }
   }
   
