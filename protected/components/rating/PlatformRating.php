a380f58d797607897d543ae727eeebfb95f5a22f
Ziga Berce <bercium@gmail.com>
2014-08-17 14:06:25 +0200

    after3h

--- protected/components/rating/PlatformRating.php
+++ protected/components/rating/PlatformRating.php
@@ -1,7 +1,7 @@
 <?php
 abstract class PlatformRating {
   protected $html = null;
-  protected $url = '';
+  protected $link = '';
   protected $id = null;
   
   abstract protected function history();
@@ -17,7 +17,7 @@ abstract class PlatformRating {
     $httpClient = new elHttpClient();
     $httpClient->setUserAgent("ff3");
     $httpClient->setHeaders(array_merge(array("Accept"=>"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"),$headers));
-    $htmlDataObject = $httpClient->get($this->url.$sufix);
+    $htmlDataObject = $httpClient->get($this->link.$sufix);
     return $htmlDataObject->httpBody;
   }
   
@@ -25,7 +25,7 @@ abstract class PlatformRating {
    * get social from the web
    */
   protected function getSocial(){
-    $social = new shareCount($this->url);
+    $social = new shareCount($this->link);
     $return = array();
     $return['twitter'] = $social->get_tweets();
     $return['linkedin'] = $social->get_linkedin();
@@ -85,7 +85,7 @@ abstract class PlatformRating {
    */
   public function analize(){
     echo "in ";
-    echo $this->url." out";
+    echo $this->link." out";
     $cws = $this->currentWebStatus();
     $this->calcContentRating($cws);
     
