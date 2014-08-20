f625dea2decf0eccfb49aedc53cb9994b8cf08c0
Ziga Berce <bercium@gmail.com>
2014-08-20 21:20:53 +0200

    Link for top projects design fix

--- protected/views/layouts/default.php
+++ protected/views/layouts/default.php
@@ -3,7 +3,7 @@
     <div class="intro-defult mb20" >
        <div class="row">
          <div class="columns large-12">
-           <a href="<?php echo Yii::app()->request->baseUrl; ?>">
+           <a href="<?php echo Yii::app()->request->baseUrl."/"; ?>">
              <h2 class="white special_title" style="margin-bottom: 0px;">
                <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png" style="height:40px"> Crowdfunding RSS
              </h2>
