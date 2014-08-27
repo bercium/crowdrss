8c4667deebefd90d63f0536a2b181a034fc6eb1b
Ziga Berce <bercium@gmail.com>
2014-08-27 23:32:50 +0200

    Stat test

--- protected/controllers/StatisticController.php
+++ protected/controllers/StatisticController.php
@@ -206,9 +206,8 @@ class StatisticController extends Controller
       $all = $all*24; // per day
       
       // less than 3 hours statisticaly too little
-      if ($h_lapsed < 4){
-        $all *= 0.8;
-      }
+      if ($h_lapsed == 3) $all *= 0.8;
+      if ($h_lapsed == 2) $all *= 0.75;
       
       if ($all < 0.15) $arrayCount[0] += 1;
       else 
