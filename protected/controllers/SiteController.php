feff3e5767452c0c45566dad6d56eccc5e80458a
Ziga Berce <bercium@gmail.com>
2014-09-08 11:05:42 +0200

    Fix sitemap link

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -375,7 +375,7 @@ EOD;
     
     
     // go trough projects newer than 1 month
-    $projects = Project::model()->findAll("time_added > :datum LIMIT 4000", array(":datum"=>date("Y-m-d H:i:s", strtotime("-1 month"))));
+    $projects = Project::model()->findAll("time_added > :datum ORDER BY id DESC LIMIT 4000", array(":datum"=>date("Y-m-d H:i:s", strtotime("-1 month"))));
     foreach ($projects as $project){
       if ($project){
         $priority = 0.35;
