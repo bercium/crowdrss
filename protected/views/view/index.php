c6ebd7eba38962d371b3319bd35ffc7c0268a1bc
brencelj <brencelj@gmail.com>
2015-04-26 18:24:21 +0200

    Update

--- protected/views/view/index.php
+++ protected/views/view/index.php
@@ -6,7 +6,7 @@ $this->pageTitle = $project->title;
 
               <div class="row">
                 <div class="columns medium-4">
-                <?php if ($project->rating){ ?>
+                <?php if (($project->rating) and ($project->deleted = 0)){ ?>
                 <span class="fa-stack fa-5x fa-lg" style="color:#0088bb; right:-20px; top:-50px; margin:0; padding:0; position: absolute;" >
                   <i class="fa fa-certificate fa-stack-1x" ></i>
                   <i style="font-size: 60%; " class="fa-stack-1x fa-inverse">
