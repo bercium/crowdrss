e69520c5c0a4b56968bbbc81cf761ce785bbc45f
Ziga Berce <bercium@gmail.com>
2015-10-25 00:51:56 +0200

    Adds implemented

--- protected/views/view/index.php
+++ protected/views/view/index.php
@@ -2,6 +2,21 @@
 if ($project->removed == 0){
     $this->pageTitle = $project->title;
 ?><div class="pt30 pb30">
+    <div class="row">
+        <div class="columns">
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- cfrss-responsive -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-0534207295672567"
+                 data-ad-slot="6740352234"
+                 data-ad-format="auto"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+    </div>
+    
       <div class="row">
         <div class="columns medium-12">
 
@@ -18,18 +33,20 @@ if ($project->removed == 0){
                 </span>
                 <?php } ?>
                   <a href="<?php echo Yii::app()->createUrl("feed/rl",array("l"=>$project->link)); ?>" trk="link_preview_<?php echo $project->id; ?>" target="_blank">
-                    <img src="<?php echo $project->image; ?>">
+                    <img src="<?php echo $project->image; ?>" style="width:100%">
                   </a>
                 </div>
                 <div class="columns medium-8">
-                  <a href="<?php echo Yii::app()->createUrl("feed/rl",array("l"=>$project->link)); ?>" trk="link_preview_<?php echo $project->id; ?>" target="_blank"><h4><?php echo $project->title; ?></h4></a>
+                  <a href="<?php echo Yii::app()->createUrl("feed/rl",array("l"=>$project->link)); ?>" trk="link_preview_<?php echo $project->id; ?>" target="_blank">
+                      <h1 class="small"><?php echo $project->title; ?></h1>
+                  </a>
                   <small><?php echo "in ".$project->origCategory->name." on ".date("D, d M Y H:i:s e",strtotime($project->time_added)); ?></small>
                   <p style="margin-top: 10px;" class="mb20">
                   <?php echo $project->description; ?>
                   <br />
                   <?php 
                     echo "<br /><strong>".$project->platform->name."</strong> - ".$project->origCategory->name." ";//." <br />";
-                    if (!empty($project->creator)) echo "<br />Creator of project: <i>".$project->creator."</i> ";
+                    if (!empty($project->creator)) echo "<br />Creator: <i>".$project->creator."</i> ";
                     //if (!empty($project->location)) $desc.= " \nCreator of project: ".$project->location;
                     if (!empty($project->goal)) echo "<br />Project goal: <strong>".$project->goal."</strong>";
                     if (!empty($project->type_of_funding)){
