56f5b6a6836469b7d92446a66717eb7b43b55bc4
brencelj <brencelj@gmail.com>
2015-11-17 12:35:01 +0100

    Fix

--- protected/controllers/CronController.php
+++ protected/controllers/CronController.php
@@ -74,6 +74,7 @@ class CronController extends Controller
     //echo $this->consoleCommand('update','pubSlush');
     echo $this->consoleCommand('update','fundAnything');
     echo $this->consoleCommand('update','fundRazr');
+    echo $this->consoleCommand('update','pledgeMusic');
   }
   
   
@@ -93,6 +94,8 @@ class CronController extends Controller
       case "fa":echo $this->consoleCommand('update','fundAnything');
         break;
       case "fr":echo $this->consoleCommand('update','fundRazr');
+        break;
+    case "pm":echo $this->consoleCommand('update','pledgeMusic');
         break;
 
       default: echo "Chose from: ks, igg, ggf, fa, fr"; //ps,
@@ -177,201 +180,5 @@ class CronController extends Controller
    */
   public function actionValidateParsers(){
     echo $this->consoleCommand('mailer','validateParsers');
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-   // Tole je za TEST
-  public function actionIndiegogo() {
-    $platform = Platform::model()->findByAttributes(array('name' => 'Indiegogo'));
-    $id = $platform->id;
-    $numberOfPages = 10;
-    $link = "https://www.indiegogo.com/explore?filter_browse_balance=true&filter_quick=new&per_page=$numberOfPages";
-    $htmlData = $this->getHtml($link, array());
-    var_dump($htmlData); Die();
-    $pattern = '/(\/projects\/.+)\/pinw/';
-    preg_match_all($pattern, $htmlData, $matches);
-    $data['links'] = $matches[1];
-    $pattern = '/src="(.+cloudinary.+)"/';
-    preg_match_all($pattern, $htmlData, $matches);
-    $data['images'] = $matches[1];
-    //var_dump($data['links']); die;
-    if (isset($data['links'])&&isset($data['images'])) {
-        $count_links = count($data['links'])-1;
-        for ($j=0; $j<= $count_links; $j++) {
-        $link = "https://www.indiegogo.com".$data['links'][$j];
-        $link = str_replace("/pinw", "", $link);
-        $link = str_replace("/qljw", "", $link);
-        $link = str_replace("/pimf", "", $link);
-        $link = str_replace("?sa=0&sp=0", "", $link);
-        $link = str_replace("?sa=0&amp;sp=0", "", $link);
-        $project_check = Project::model()->find("link LIKE :link1  OR  link LIKE :link2  OR  link LIKE :link3  OR  image LIKE :image ",
-                                                array(':link1' => str_replace("?sa=0&sp=0", "", $data['links'][$j]),
-                                                      ':link2' => $data['links'][$j], 
-                                                      ':image' => $data['images'][$j], 
-                                                      ':link3' => $link));
-        if (!$project_check) {
-          $htmlData = $this->getHtml($link, array(), 'igg');
-          var_dump($data_single); die;
-          $htmlData .= $this->getHtml($link . "/show_tab/home", array("X-Requested-With" => "XMLHttpRequest", 'igg'));
-          $data_single = $this->parseIndiegogo($htmlData);
-          var_dump($data_single); die;
-	  if ($data_single == false) { continue; }
-          $insert = new Project;
-          $insert->title = $data_single['title'];
-          $insert->description = $data_single['description'];
-          $insert->image = $data['images'][$j];
-          $insert->link = $link;
-          $insert->time_added = date("Y-m-d H:i:s");
-          $insert->platform_id = $id;
-          $category = $this->checkCategory($data_single['category'], $link, "");
-          $insert->orig_category_id = $category->id;
-          if (isset($data_single['start_date']))
-            $insert->start = $data_single['start_date'];
-          if (isset($data_single['end_date']))
-            $insert->end = $data_single['end_date'];
-          if (isset($data_single['goal']))
-            $insert->goal = $data_single['goal'];
-          if (isset($data_single['location']))
-            $insert->location = $data_single['location'];
-          if (isset($data_single['type_of_funding'])) {
-            if ($data_single['type_of_funding'] == "Fixed Funding") {
-              $typeOfFunding = 0;
-            } else {
-              $typeOfFunding = 1;
-            }
-            $insert->type_of_funding = $typeOfFunding;
-          }
-          var_dump($insert); die;
-          $insert->save();
-
-
-          $id_project = $insert->id;
-          // Category add
-          $insert_category = new ProjectOrigcategory;
-          $insert_category->project_id = $id_project;
-          $category = $this->checkCategory($data_single['category'], $link, "");
-	  $insert_category->orig_category_id = $category->id;
-	  $insert_category->save();
-          
-          // get rating 
-          $IggRating = new IndiegogoRating($link, $insert->id, $htmlData);
-          $rating = $IggRating->firstAnalize();
-          $insert->rating = $rating;
-          $insert->save();          
-//          print_r($insert->getErrors());
-        }
-      }
-    }
-  }
-  
-  function parseIndiegogo($htmlData) {
-    
-    $pattern = '/var utag_data = (.+);/'; 
-    preg_match($pattern, $htmlData, $match);
-    if (isset($match[1])){$json = html_entity_decode($match[1]);}
-    else{return false;}
-    $json = str_replace('\\"', "", $json);
-    $json = str_replace('\"', "", $json);
-    $jsonData = json_decode($json);
-    if ($jsonData == null){ return false; }
-    if ($jsonData->page_name == "Invalid Page | Indiegogo") {return false;}
-    
-    // Title
-    $data['title'] = $jsonData->campaign_name;
-            
-    // Description
-    $data['description'] = $jsonData->campaign_description;
-            
-    // Category
-    $data['category'] = $jsonData->campaign_category;
-
-    // Goal
-    $money = Yii::app()->numberFormatter->formatCurrency($jsonData->{'campaign_goal_amount'}, $jsonData->{'site_currency'});
-    $money_split = explode(".", $money);
-    if ($money_split[1] == "00") {$data['goal'] = $money_split[0];
-    } else {$data['goal'] = $money;}
-
-    // Type of funding
-    if ($jsonData->{'campaign_type'} == "flexible_funding") {$data['type_of_funding'] = 1;}
-    else{$data['type_of_funding'] = 0;}
-
-    // Start date
-    $data['start_date'] = date("Y-m-d H:i:s", strtotime($jsonData->{'campaign_start_date'}));
-
-    // End date
-    $data['end_date'] = date("Y-m-d H:i:s", strtotime($jsonData->{'campaign_end_date'}));
-
-    // Location
-    $pattern = '/gon.ga_impression_data=(.+);gon.env=/';
-    preg_match($pattern, $htmlData, $match);
-    $json = html_entity_decode($match[1]);
-    $json = str_replace('\\"', "\'", $json);
-    $jsonData = json_decode($json);
-    if ($jsonData != null) { $data['location'] = $jsonData->{'list'}; }
-
-    return($data);
-  }
-  
-  function getHtml($link, $header) {
-    $httpClient = new elHttpClient();
-    $httpClient->enableRedirects();
-    $httpClient->setProxy("111.161.126.98", 80);
-    $httpClient->setUserAgent("ff3");
-    $httpClient->setHeaders(array("Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
-    $htmlDataObject = $httpClient->get($link, $header);
-    return $htmlDataObject->httpBody;
-  }
-  
-  function checkCategory($category_check, $link, $platform){
-    $category_check = preg_replace_callback("/(&#[0-9]+;)/", function($m) { return mb_convert_encoding($m[1], "UTF-8", "HTML-ENTITIES"); }, $category_check);
-    if ($platform == "PledgeMusic") {
-      $category = OrigCategory::model()->findByAttributes(array('name' => $category_check, 'category_id' => '15'));
-    }elseif ($platform == "PubSlush"){
-      $category = OrigCategory::model()->findByAttributes(array('name' => $category_check, 'category_id' => '24'));
-    }else{
-      $category = OrigCategory::model()->findByAttributes(array('name' => $category_check));
-    }
-    if ($category) {
-      return $category;
-    } else {
-      $updateOrigCategory = new OrigCategory();
-      if ($platform == "PledgeMusic") {
-        $updateOrigCategory->category_id = 15;
-      }elseif ($platform == "PubSlush"){
-        $updateOrigCategory->category_id = 24;
-      }
-      $updateOrigCategory->name = $category_check;
-      $updateOrigCategory->save();
-      if ($platform == "PledgeMusic") {
-        $category = OrigCategory::model()->findByAttributes(array('name' => $category_check, 'category_id' => '15'));
-      }elseif ($platform == "PubSlush"){
-        $category = OrigCategory::model()->findByAttributes(array('name' => $category_check, 'category_id' => '24'));
-      }else{
-        $category = OrigCategory::model()->findByAttributes(array('name' => $category_check));
-      }
-      //$this->errorMail($link, $category_check, $category->id);
-      return $category;
-    }
-  }
-  
-  function errorMail($link, $category, $id) {
-    $message = new YiiMailMessage;
-    $message->view = 'system';
-    $message->subject = 'Missing original category';
-    $content = 'Category: ' . $category . '<br>Id: ' . $id . '<br>Link to project: ' . $link;
-    $message->setBody(array("content" => $content, "title" => "Added new original category"), 'text/html');
-    $message->to = Yii::app()->params['scriptEmail'];
-    $message->from = Yii::app()->params['noreplyEmail'];
-    Yii::app()->mail->send($message);
-  }
-  
-    
+  }    
 }
