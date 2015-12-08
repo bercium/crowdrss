c5dca602d778d953f4ac682438a87b892229a881
Ziga Berce <bercium@gmail.com>
2015-12-08 22:45:01 +0100

    Come back title

--- js/app.js
+++ js/app.js
@@ -35,6 +35,48 @@
 })(jQuery, this);
 
 
+(function() {
+  var hidden = "hidden";
+  var title = '';
+
+  // Standards:
+  if (hidden in document)
+    document.addEventListener("visibilitychange", onchange);
+  else if ((hidden = "mozHidden") in document)
+    document.addEventListener("mozvisibilitychange", onchange);
+  else if ((hidden = "webkitHidden") in document)
+    document.addEventListener("webkitvisibilitychange", onchange);
+  else if ((hidden = "msHidden") in document)
+    document.addEventListener("msvisibilitychange", onchange);
+  // IE 9 and lower:
+  else if ("onfocusin" in document)
+    document.onfocusin = document.onfocusout = onchange;
+  // All others:
+  else
+    window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onchange;
+
+  function onchange (evt) {
+    if (title == '') title = document.title;
+    var v = "visible", h = "hidden",
+        evtMap = {
+          focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
+        };
+
+    evt = evt || window.event;
+    if (evt.type in evtMap)
+      type = evtMap[evt.type];
+    else
+      type = this[hidden] ? "hidden" : "visible";
+
+    if (type == 'hidden') document.title = 'Hey come back!';
+    else document.title = title;
+  }
+
+  // set the initial state (but only if browser supports the Page Visibility API)
+  if( document[hidden] !== undefined )
+    onchange({type: document[hidden] ? "blur" : "focus"});
+})();
+
 
 function contact(e){
 	var pri = "@";
