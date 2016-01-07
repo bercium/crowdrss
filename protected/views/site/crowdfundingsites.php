a8e44c117673176c397d7af2dc3ce317c76361d0
Ziga Berce <bercium@gmail.com>
2016-01-08 00:55:41 +0100

    Fix layout

--- protected/views/site/crowdfundingsites.php
+++ protected/views/site/crowdfundingsites.php
@@ -4,7 +4,7 @@
         <ul class="side-nav" role="navigation" title="Link List">
             <li role="menuitem"><a href="<?php echo Yii::app()->createUrl("crowdfunding-sites"); ?>">All</a></li>
             <?php foreach ($categories as $cat) {?>
-                <li role="menuitem"><a href="<?php echo Yii::app()->createUrl("crowdfunding-sites/".$cat['category']); ?>"><?php echo $cat['category']; ?></a></li>
+                <li role="menuitem"><a href="<?php echo Yii::app()->createUrl("crowdfunding-sites/".$cat['category']); ?>"><?php echo $cat['category']; ?></a><hr></li>
             <?php } ?>
         </ul>
     </div>
@@ -30,7 +30,7 @@
             if ($cur_sub_cat != $site['category'].'-'.$site['sub_category']){
                 $cur_sub_cat = $site['category'].'-'.$site['sub_category'];
                 
-                if(!$firsttime){ ?></ul><?php } ?>        
+                if(!$firsttime){ ?></ul><hr><?php } ?>        
                 <h3 data-magellan-destination="<?php echo str_replace(' ','',$site['sub_category']); ?>"><?php echo $site['sub_category']; ?></h3>
                 <a name="<?php echo str_replace(' ','',$site['sub_category']); ?>"></a>
 
@@ -40,7 +40,7 @@
             }
             ?>
         
-            <li><img src="<?php echo getLinkIcon($site['link']); ?>"> <a href="<?php echo $site['link']; ?>" target="_blank"><?php echo $site['title']; ?></a></li>
+            <li><img src="<?php echo getLinkIcon($site['link']); ?>"> <a href="<?php echo $site['link']; ?>" target="_blank"  data-tooltip aria-haspopup="true" class="has-tip" title="<?php echo $site['title']."<br />".$site['keywords']; ?>"><?php echo trim_text($site['title'],20); ?></a></li>
         
     
         <?php } ?>
